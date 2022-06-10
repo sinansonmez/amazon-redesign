df04650dbd09d351ed2b9ce3409c348519024a25
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-10 22:55:08 +0000

    router setup is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Cart from './pages/Cart';
+import Photos from './pages/Photos';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <h1>Home Page</h1>
+
+      <Routes>
+        <Route path='/' element={<Photos />}>
+          <Route path='cart' element={<Cart />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
