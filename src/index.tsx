1914672f8f098213328fd2e2cc88818a3c4e1941
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-11 10:41:36 +0000

    context setup completed

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './Context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
