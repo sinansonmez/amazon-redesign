b9d66cc34d71bc6fb68d59820f18ad87cb27479a
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-11 11:12:10 +0000

    photos fetched and saved to state

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Context = React.createContext('');
+const Context = React.createContext({});
+
+const API_URL = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+
+type Photos = {
+  id: string,
+  isFavorite: boolean,
+  url: string
+};
 
 const ContextProvider = (props: any) => {
+  const [photos, setPhotos] = useState<Photos[]>([])
+
+  useEffect(() =>{
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setPhotos(data))
+  },[])
+
   return (
-    <Context.Provider value='' >
+    <Context.Provider value={{photos}} >
       {props.children}
     </Context.Provider>
   )
