a2019eef6f78efa3a6d6d6a8b6e121cf90d32b9c
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-12 19:44:25 +0000

    image is added to the cart

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -3,13 +3,17 @@ import Image from "../components/Image";
 import { Context } from "../Context";
 
 const Photos = () => {
-    const { photos, toggleFavorite } = useContext(Context)
-    const photosElement = photos.map(photo => (
+    const { photos, toggleFavorite, updateCart, cartItems } = useContext(Context)
+    const photosElement = photos.map(photo => {
+        const inCart = cartItems.find(item => item.id === photo.id)
+        return (
         <Image
             key={photo.id}
             img={photo}
+            inCart={inCart ? true : false}
             toggleFavorite={toggleFavorite}
-        />))
+            updateCart={updateCart}
+        />)})
 
     return (
         <main className="photos" >
