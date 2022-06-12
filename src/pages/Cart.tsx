8ee1c9d05b85f7c914046a39fd7cc5475e6571e8
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-12 22:33:44 +0000

    cart page is completed

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import CartItem from "../components/CartItem";
+import { Context } from "../Context";
 
 const Cart = () => {
+    const { cartItems, clearCart } = useContext(Context)
+    const [ordered, setOrdered] = useState(false)
+
+    const cartItemElements = cartItems.map(item => <CartItem item={item} />)
+    const totalCost = cartItems.length * 5.99
+    const totalCostDisplay = totalCost.toLocaleString("en-US", { style: "currency", currency: "USD" })
+
+    const completeOrder = () => {
+        setOrdered(true)
+        setTimeout(() => {
+            setOrdered(false)
+            clearCart()
+        }, 3000)
+    }
+
+    const placeOrderButton = cartItems.length > 0 && (<div className="order-button">
+        <button onClick={completeOrder} >{ordered ? 'Ordering...' : 'Place Order'}</button>
+    </div>)
+
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total: {totalCostDisplay}</p>
+            {placeOrderButton}
         </main>
     )
 }
