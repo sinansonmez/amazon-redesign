ed0dab18a93ab9510da9de2e422ce76b7ce897fc
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-13 11:39:00 +0000

    sass is included

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Header from './components/Header';
 import Cart from './pages/Cart';
 import Photos from './pages/Photos';
