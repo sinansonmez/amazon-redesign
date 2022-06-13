3c9b3f9c3784cd0cc647c929cd115a9b110c3d1b
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-06-13 21:41:40 +0000

    menu style is fixed

--- src/components/DropdownMenu/MenuItems.tsx
+++ src/components/DropdownMenu/MenuItems.tsx
@@ -13,30 +13,32 @@ const MenuItems = ({ title, items }: Props) => {
 
   useEffect(() => {
     const handler = (event: any) => {
-     if (dropdown && ref.current && !ref.current.contains(event.target)) {
-      setDropdown(false);
-     }
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
     };
     document.addEventListener("mousedown", handler);
     document.addEventListener("touchstart", handler);
     return () => {
-     // Cleanup the event listener
-     document.removeEventListener("mousedown", handler);
-     document.removeEventListener("touchstart", handler);
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
     };
-   }, [dropdown]);
+  }, [dropdown]);
 
   return (
     <li className="menu-items" ref={ref} >
-          <button
-            type="button"
-            aria-haspopup="menu"
-            aria-expanded={dropdown ? "true" : "false"}
-            onClick={() => setDropdown((prev) => !prev)}
-          >
-            {title}{" "}
-          </button>
-          <Dropdown submenus={items} dropdown={dropdown} />
+      <p className="menu-items__subtitle" >Select</p>
+      <button
+        type="button"
+        className="menu-items__title"
+        aria-haspopup="menu"
+        aria-expanded={dropdown ? "true" : "false"}
+        onClick={() => setDropdown((prev) => !prev)}
+      >
+        {title}{" "}
+      </button>
+      <Dropdown submenus={items} dropdown={dropdown} />
     </li>
   );
 };
